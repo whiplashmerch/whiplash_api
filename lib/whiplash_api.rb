82e1e406e894754d6c4c1f67a4c2d471a7f0b671
Mark Dickson <mark@sitesteaders.com>
2012-09-12 18:06:08 -0400

    changed name

--- lib/whiplash_api.rb
+++ lib/whiplash_api.rb
@@ -1,10 +1,6 @@
 require "rubygems"
 require 'active_resource'
 
-module WhiplashAPI
-
-end
-
 require 'whiplash_api/base'
 require 'whiplash_api/item'
 require 'whiplash_api/order'
--- test/helper.rb
+++ test/helper.rb
@@ -12,7 +12,7 @@ require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'whiplash'
+require 'whiplash_api'
 
 class Test::Unit::TestCase
 end
