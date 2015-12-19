0e77f1ff45bc1e631626e05df6d03e0d1ffce76f
Nikhil Gupta <me@nikhgupta.com>
2015-12-19 15:51:57 +0530

    fixed failing tests on travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,23 @@ require 'whiplash_api'
 
 module WhiplashApi
   module TestHelpers
+    def self.setup!
+      version = (ENV['WL_API_VERSION'] || WhiplashApi::DEFAULT_API_VERSION).to_i
+      raise "Please, set a valid API version" if version == 0
+      raise "Please, set environment variable WL_API_KEY" if version == 1 && !ENV['WL_API_KEY']
+      raise "Please, set environment variable WL_OAUTH_KEY" if version > 1 && !ENV['WL_OAUTH_KEY']
+      WhiplashApi::Base.testing!
+      WhiplashApi::Base.api_version = version
+      WhiplashApi::Base.api_key = version == 1 ? ENV['WL_API_KEY'] : ENV['WL_OAUTH_KEY']
+    end
+
     def self.teardown!
       return if ENV['NO_TEARDOWN'].present?
+      setup!
+
+      puts "Removing/cancelling resources remaining from previous/current tests..."
+      puts "This may take a while..."
+
       # remove all created items
       skus = %w{SOME-SKU-KEY SOME-SKU-KEY-2 SOME-SKU-KEY-3 SOME-SKU-KEY-4}
       skus.map do |sku|
@@ -28,20 +43,15 @@ end
 
 RSpec.configure do |config|
   config.before(:each) do
-    version = (ENV['WL_API_VERSION'] || WhiplashApi::DEFAULT_API_VERSION).to_i
-    raise "Please, set a valid API version" if version == 0
-    raise "Please, set environment variable WL_API_KEY" if version == 1 && !ENV['WL_API_KEY']
-    raise "Please, set environment variable WL_OAUTH_KEY" if version > 1 && !ENV['WL_OAUTH_KEY']
-    WhiplashApi::Base.testing!
-    WhiplashApi::Base.api_version = version
-    WhiplashApi::Base.api_key = version == 1 ? ENV['WL_API_KEY'] : ENV['WL_OAUTH_KEY']
+    WhiplashApi::TestHelpers.setup!
+  end
+
+  config.before(:suite) do
+    WhiplashApi::TestHelpers.teardown!
   end
 
   config.after(:suite) do
     puts
-    puts "Removing/cancelling resources created while testing..."
-    puts "This may take a while..."
-
     WhiplashApi::TestHelpers.teardown!
   end
 end
